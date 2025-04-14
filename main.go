6bce20bbb6bd3f6cd1f342df3ab95c6754435f83
WM <wmrngcd@gmail.com>
2025-04-14 07:46:20 +0300

    upstream update

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"go-3-mini/lib"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -13,26 +11,17 @@ func main() {
 	lib.ConnectDB()
 	lib.InitCache() // Initialize the cache
 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("☑️ Choose an option:")
-	fmt.Println("1️⃣. Start HTTP server")
-	fmt.Println("2️⃣. Start interactive shell")
-	fmt.Print("Enter your choice (1 or 2): ")
-
-	choice, _ := reader.ReadString('\n')
-	choice = strings.TrimSpace(choice)
-
-	switch choice {
-	case "1":
-		fmt.Println("🚀 Starting HTTP server on port 8080...")
-		if err := lib.StartServer("8080"); err != nil {
-			fmt.Printf("⛔ Failed to start server: %v\n", err)
-			os.Exit(1)
-		}
-	case "2":
+	// Check command-line arguments
+	if len(os.Args) > 1 && os.Args[1] == "shell" {
+		fmt.Println("💬 Starting interactive shell...")
 		lib.StartShell()
-	default:
-		fmt.Println("⛔ Invalid choice. Please run again and enter either 1 or 2.")
+		return
+	}
+
+	// Default to starting the HTTP server
+	fmt.Println("🚀 Starting HTTP server on port 8080...")
+	if err := lib.StartServer("8080"); err != nil {
+		fmt.Printf("⛔ Failed to start server: %v\n", err)
 		os.Exit(1)
 	}
 }
